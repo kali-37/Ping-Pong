0789c8ef00888765b151c73deecd4755872d7111
Subash1237 <rimalsubash77@gmail.com>
2024-01-02 22:35:09 +0545

    Movable part and responsivness part completed.

--- src/main.c
+++ src/main.c
@@ -9,13 +9,13 @@
 //#include<SDL2/SDL_image.h>
 //#include<SDL2/SDL_ttf.h>
 
-static Uint32 window_Height =1000;
-static Uint32 window_Width= 1000;
-// static Uint32 cord_x;
+static Uint32 window_Height =0;
+static Uint32 window_Width= 0;
+static Sint32 cord_x=0;
 // static Uint32 cord_y;
-static Uint32 _lasttick;
+static Sint32 _lasttick;
 
-#define FPS 60
+#define FPS 90
 SDL_Renderer *_render ;
 void renderer(SDL_Window *win);
 SDL_FRect  _playable;
@@ -26,30 +26,36 @@ SDL_FRect  _playable;
 
 
 void update_playable(){
-_playable.h=(float)window_Height/30;
-_playable.w=(float)window_Width /10;
-_playable.x=(float)30;
-_playable.y=(float)10;
+_playable.h=(float)window_Height*0.03;
+_playable.w=(float)window_Width *0.11;
+printf("%d   \n",cord_x);
+if (cord_x<0) cord_x=1;
+if (cord_x>window_Width-_playable.w) cord_x=window_Width-_playable.w;
+_playable.x=(float)cord_x;   // only this changes on movable..
+// if (0<_playable.x<window_Width){
+
+// }
+_playable.y=(float)window_Height*0.9;
 }
 
 
 
 
     
-void _eventcheck(){
+void _eventcheck(SDL_Window *win){
     SDL_DisplayMode dm;
-        if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
+    if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
     SDL_Log("SDL_GetDesktopDisplayMode failed: %s", SDL_GetError());
     exit(0);
 }
-printf("MAX DWM %d\n",dm.h);
-        printf("WINDOW HEIGHT %u \n", window_Height);
-        if (window_Height>dm.h*0.8 && window_Width>dm.w*0.8){
-            window_Height=dm.h*0.8;
-            window_Width=dm.w*0.8;
+        if (window_Height<dm.h*0.8 && window_Width<dm.w*0.8){
+            window_Height=dm.w*0.8;
+            window_Width=dm.h*0.9;
+            SDL_SetWindowSize(win,window_Height,window_Width);
         }
        update_playable();
-       SDL_SetRenderDrawColor(_render, 250,0,0,255) ;
+       SDL_RenderClear(_render);
+       SDL_SetRenderDrawColor(_render, 0,0,0,255) ;
        SDL_RenderDrawRectF(_render,&_playable);
        SDL_RenderClear(_render);
        SDL_SetRenderDrawColor(_render, 0,255,0,255) ;
@@ -63,9 +69,9 @@ void renderer(SDL_Window* win){
     SDL_Event event;
     bool  run=true;
     while(run){
-        _eventcheck();
-        SDL_RenderPresent(_render);
        _lasttick=SDL_GetTicks();
+        _eventcheck(win);
+        SDL_RenderPresent(_render);
         while(SDL_PollEvent(&event)){
             if(event.type==SDL_QUIT){
                 run=false;
@@ -75,14 +81,25 @@ void renderer(SDL_Window* win){
                     case SDL_WINDOWEVENT_RESIZED:
                     window_Width=event.window.data1;
                     window_Height=event.window.data2;
-                printf("Window resized, new dimensions: %d x %d\n", window_Width, window_Height);
 
                     SDL_RenderClear(_render);
                     break;
                 }
             }
-        }
-        uint32_t computed=_lasttick+FPS-SDL_GetTicks();
+            else if(event.type==SDL_KEYDOWN){
+                switch(event.key.keysym.sym){
+                   case SDLK_LEFT :
+                   cord_x-=55;
+                   break;
+                   case SDLK_RIGHT:
+                   cord_x+=55;
+                   break; 
+                }
+                }
+
+            }
+        Sint32 computed=_lasttick+1000/FPS-SDL_GetTicks();
+        printf("COMPUTED ,%d",computed);
         if (computed>0){
         SDL_Delay(computed);
         }
